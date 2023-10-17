b9106828a33886268afd28c5e750cb05e294ef4e
Maaz_Khan <maazkhan0348810@gmail.com>
2023-10-18 01:27:42 +0500

    prompt to user to take input

--- dataPlan.c
+++ dataPlan.c
@@ -2,4 +2,10 @@
 int main(){
     float monthlyPackage,usedPackage,averagePackageUsedDaily,dailyUsedPackage,remainingPackageUsedDaily,packageExceeding,remainingPackage;
     int days,remainingDays;
+      printf("Enter your monthly package:");
+    scanf("%f",&monthlyPackage);
+    printf("How many days you used Package:");
+    scanf("%i",&days);
+    printf("How much Gb of your Package you used:");
+    scanf("%f",&usedPackage);
 }
