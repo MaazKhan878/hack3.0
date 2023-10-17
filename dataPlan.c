76e8246bfc5f060817bf97b7c57b38e4fc7d17ab
Maaz_Khan <maazkhan0348810@gmail.com>
2023-10-18 01:26:23 +0500

    made variable

---
+++ dataPlan.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    float monthlyPackage,usedPackage,averagePackageUsedDaily,dailyUsedPackage,remainingPackageUsedDaily,packageExceeding,remainingPackage;
+    int days,remainingDays;
+}
