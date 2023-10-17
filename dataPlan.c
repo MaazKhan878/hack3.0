799273b8a759e5540904e8ce72ec13b4f03183d4
Maaz_Khan <maazkhan0348810@gmail.com>
2023-10-18 01:33:55 +0500

    added second condition to program which different result according the use input

--- dataPlan.c
+++ dataPlan.c
@@ -21,4 +21,11 @@ int main(){
      printf("You are EXCEEDING your average daily use ( %f GB/day)\n",exceedDailyUsed);
      printf("Continuing this high usage, you'll exceed your data plan by %f GB\n",packageExceeding);
      printf("To stay below your data plan, use no more than %f GB/day\n", remainingPackage);
-}
+    } else if(usedPackage==0){
+     averagePackageUsedDaily=monthlyPackage/30;
+     printf("_____________________________________________\n");
+     printf("%i days used, %i days remaining\n",days,remainingDays);
+     printf("Average daily use: %f GB/day\n",dailyUsedPackage);
+     printf("You are at or below your average daily use (%f GB/day)\n",averagePackageUsedDaily);
+     printf("You can use up to %f GB/day and stay below your data plan limit\n",remainingPackageUsedDaily);
+    }
